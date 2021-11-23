a65ca2c1781a3f7ee5c54b7630b60e896418f065
Nicolas <nico.chabanpavon@gmail.com>
2021-11-23 17:07:32 -0300

    OK: custom hooks for currencies

---
+++ src/hooks/useCryptomoneda.js
@@ -0,0 +1,53 @@
+import {Fragment , useState} from 'react';
+import styled from '@emotion/styled';
+
+const Label = styled.label`
+    font-family: 'Bebas Neue', coursive;
+    color: #FFF;
+    text-transform: uppercase;
+    font.weight: bold;
+    font-size: 2.4rem;
+    margin-top:2rem;
+    display:block;
+`;
+
+const Select = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+    text-align: center;
+    font-size: 1.2rem;
+
+`;
+
+const useCrypto = (label,stateInicial, opciones)=>{
+
+    // state del custom hook
+    const [state, actualizarState] = useState(stateInicial);
+
+    //función que se imprimirá en pantalla
+    const Seleccionar = ()=>(
+        <Fragment>
+            <Label >{label}</Label>
+            <Select
+                onChange={ e => actualizarState(e.target.value)}
+                value={state}
+            >
+                <option value=""> --Seleccione--</option>
+                {opciones.map(opcion=>(
+                    <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
+                ))}
+            </Select>
+        </Fragment>
+    )
+
+    //retornar state, interfaz y fn que modifique el state
+
+    return [ state, Seleccionar, actualizarState ];
+
+}
+
+export default useCrypto;
