357e5340fa2c34cd5850bca420b730ce8a1cc1aa
Nicolas <nico.chabanpavon@gmail.com>
2021-11-23 12:22:42 -0300

    OK: added component Formulario

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,55 @@
+import styled from '@emotion/styled';
+import useMoneda from '../hooks/useMoneda';
+
+const Boton = styled.input`
+
+    margin-top: 20px;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 10px;
+    background-color: #66a2fe;
+    border: none;
+    width: 100%;
+    border-radius: 10px;
+    color: #FFF;
+    transition: background-color .3s ease;
+
+    &:hover {
+        background-color: #326AC0;
+        cursor: pointer;
+    }
+
+
+`
+
+const Formulario = () => {
+
+    const MONEDAS = [
+        { codigo: 'USD', nombre: 'Dolar de Estados Unidos' },
+        { codigo: 'MXN', nombre: 'Peso Mexicano' },
+        { codigo: 'ARS', nombre: 'Peso Argentino' },
+        { codigo: 'EUR', nombre: 'Euro' },
+        { codigo: 'GBP', nombre: 'Libra Esterlina' }
+    ];
+
+    //utilizar useMoneda
+    const[moneda, SelectMonedas] = useMoneda('Elige tu moneda', '', MONEDAS)
+
+    return (  
+
+        <form>
+
+            <SelectMonedas/>
+
+
+            <Boton
+            type="submit"
+            value="calcular"
+            />
+
+        </form>
+        
+    );
+}
+ 
+export default Formulario;
